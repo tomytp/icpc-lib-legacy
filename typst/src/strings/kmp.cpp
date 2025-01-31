8cb9a16b115d129b208b5e5040e682c0a695c5de
tomytp <thomazrebellomiranda@gmail.com>
2025-01-31 00:38:21 -0300

    feat: kmp revised

--- typst/src/strings/kmp.cpp
+++ typst/src/strings/kmp.cpp
@@ -1,18 +1,31 @@
-v64 pi(const string& s) {
-    ll n = s.size();
-	v64 p(n);
-	forn(i, 1, n) {
-		ll g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
-		p[i] = g + (s[i] == s[g]);
+v64 pi(string& s) {
+	v64 p(s.size());
+	for (int i = 1, j = 0; i < s.size(); i++) {
+		while (j and s[j] != s[i]) j = p[j-1];
+		if (s[j] == s[i]) j++;
+		p[i] = j;
 	}
 	return p;
 }
 
-v64 match(const string& s, const string& pat) {
-	v64 p = pi(pat + '\0' + s), res;
-	ll pz = p.size(), sz = s.size(), patz = pat.size();
-    forn(i, pz - sz, pz)
-		if (p[i] == patz) res.push_back(i - 2 * patz);
-	return res;
+v64 match(string& s, string& t) {
+	v64 p = pi(s), match;
+	for (int i = 0, j = 0; i < t.size(); i++) {
+		while (j and s[j] != t[i]) j = p[j-1];
+		if (s[j] == t[i]) j++;
+		if (j == s.size()) match.push_back(i-j+1), j = p[j-1];
+	}
+	return match;
 }
+
+struct KMPaut : vector<v64> {
+	KMPaut(){}
+	KMPaut (string& s) : vector<v64>(26, v64(s.size()+1)) {
+		v64 p = pi(s);
+		auto& aut = *this;
+		aut[s[0]-'a'][0] = 1;
+		for (char c = 0; c < 26; c++)
+			for (int i = 1; i <= s.size(); i++)
+				aut[c][i] = s[i]-'a' == c ? i+1 : aut[c][p[i-1]];
+	}
+};
