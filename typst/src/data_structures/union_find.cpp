45076005085470660321f1acf1eda8cec0eba587
tomytp <thomazrebellomiranda@gmail.com>
2025-03-20 20:11:28 -0300

    dsu

--- typst/src/data_structures/union_find.cpp
+++ typst/src/data_structures/union_find.cpp
@@ -1,11 +1,11 @@
 struct dsu {
-	vector<int> id, sz;
+	vector<ll> id, sz;
 
-	dsu(int n) : id(n), sz(n, 1) { iota(id.begin(), id.end(), 0); }
+	dsu(ll n) : id(n), sz(n, 1) { iota(id.begin(), id.end(), 0); }
 
-	int find(int a) { return a == id[a] ? a : id[a] = find(id[a]); }
+	ll find(ll a) { return a == id[a] ? a : id[a] = find(id[a]); }
 
-	void unite(int a, int b) {
+	void unite(ll a, ll b) {
 		a = find(a), b = find(b);
 		if (a == b) return;
 		if (sz[a] < sz[b]) swap(a, b);
