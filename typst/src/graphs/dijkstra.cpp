e5c2ee5511f41371e49382046656d20b9aaad15a
tomytp <thomazrebellomiranda@gmail.com>
2025-03-26 14:47:30 -0300

    dijkstra

--- typst/src/graphs/dijkstra.cpp
+++ typst/src/graphs/dijkstra.cpp
@@ -1,29 +1,29 @@
-vector<vp64> adj; // (v, w)
+vector<vector<p64>> g;
 
-// d = distance | p = path
+// d = distance | p = from/path
 void dijkstra(ll s, v64 &d, v64& p) {
-    int n = adj.size();
+    ll n = g.size();
     d.assign(n, INF);
     p.assign(n, -1);
 
     d[s] = 0;
-    priority_queue<p64, vp64, greater<p64>> pq;
+    priority_queue<p64> pq;
     pq.push({0, s});
     while (!pq.empty()) {
         ll u = pq.top().second;
-        ll d_u = pq.top().first;
+        ll d_u = -pq.top().first;
         pq.pop();
 
         if (d_u != d[u]) continue;
 
-        for (auto edge : adj[u]) {
+        for (auto edge : g[u]) {
             ll v = edge.first;
             ll w_v = edge.second;
 
             if (d[u] + w_v < d[v]) {
                 d[v] = d[u] + w_v;
                 p[v] = u;
-                pq.push({d[v], v});
+                pq.push({-d[v], v});
             }
         }
     }
