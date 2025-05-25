2359ee365fcd233cead0b8a7fb7b6e90b3f84a33
MiguelABatista <miguel260503@gmail.com>
2025-05-25 18:38:00 -0300

    mod_functions

--- typst/src/math/mod_functions.cpp
+++ typst/src/math/mod_functions.cpp
@@ -1,31 +1,17 @@
 const ll MOD = 1'000'000'007;
 
-ll mult(ll a, ll b){
-    return ((a%MOD)*(b%MOD))%MOD;
-}
+inline ll sum(ll a, ll b) { a += b; if (a >= MOD) a -= MOD; return a; }
+inline ll sub(ll a, ll b) { a -= b; if (a < 0)  a += MOD; return a; }
+inline ll mult(ll a, ll b) { return (a * b) % MOD; }
 
-ll sum(ll a, ll b){
-    return ((a%MOD)+(b%MOD))%MOD;
-}
-
-ll sub(ll a, ll b){
-    return ((a%MOD) - (b%MOD) + MOD)%MOD;
-}
-
-ll pot(ll a, ll b){
-    ll expo=1, prod=a;
-    b = b % (MOD-1);
-    a = a % MOD;
-
-    while(b>=1){
-        if(b%2 == 1) expo = mult(expo,prod);
-        prod = mult(prod,prod);
-        b = b/2;
+inline ll pot(ll base, ll exp) {
+    ll res = 1;
+    while (exp) {
+        if (exp & 1) res = mult(res, base);
+        base = mult(base, base);
+        exp >>= 1;
     }
-    
-    return expo;
+    return res;
 }
 
-ll inv_mod(ll a){
-    return pot(a, MOD-2);
-}
+inline ll inv_mod(ll a) {return pot(a, MOD-2);}
