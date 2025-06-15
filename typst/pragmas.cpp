87b68870eba437b3b9883e35118003d03fe8a7f4
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2025-06-15 11:39:10 -0300

    Update pragmas.cpp

--- typst/pragmas.cpp
+++ typst/pragmas.cpp
@@ -1,11 +1,9 @@
 
 // Perfomance geral (seguro p/ CP)
  #pragma GCC optimize("O3,unroll-loops,fast-math")
- #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
  
 // Máximo vetor + FP agressivo (pode quebrar precisão)
  #pragma GCC optimize("Ofast,fast-math,unroll-loops,inline")
- #pragma GCC target("avx2,fma,sse4.2,popcnt,tune=native")
 
 // Foco em binário pequeno
  #pragma GCC optimize("Os")
