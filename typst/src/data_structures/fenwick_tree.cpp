16f5d22ce153ae87b3c1bd6fc8207ff9925a9185
tomytp <thomazrebellomiranda@gmail.com>
2025-06-25 15:44:00 -0300

    bit

--- typst/src/data_structures/fenwick_tree.cpp
+++ typst/src/data_structures/fenwick_tree.cpp
@@ -1,21 +1,30 @@
-//1-indexed
-struct BIT {
-    v64 ft;
-    BIT(ll n) {
-        ft.assign(n+1, 0);
-    }
-
-    ll rsq(ll i) {
-        ll sum = neutral;
-        for (;i; i-=(i&-i)) sum = comp(sum, ft[i]);
-        return sum;
-    }
-
-    ll rsq(ll i, ll j) {
-        return rsq(j) - rsq(i-1);
-    }
-
-    void add(ll i, ll v) {
-        for(;i<(ll)ft.size(); i+= (i&-i)) ft[i] = comp(ft[i], v);
-    }
+struct Bit {
+	ll n;
+	v64 bit;
+	Bit(ll _n=0) : n(_n), bit(n + 1) {}
+	Bit(v64& v) : n(v.size()), bit(n + 1) {
+		for (ll i = 1; i <= n; i++) {
+			bit[i] += v[i - 1];
+			ll j = i + (i & -i);
+			if (j <= n) bit[j] += bit[i];
+		}
+	}
+	void update(ll i, ll x) { // soma x na posicao i
+		for (i++; i <= n; i += i & -i) bit[i] += x;
+	}
+	ll pref(ll i) { // soma [0, i]
+		ll ret = 0;
+		for (i++; i; i -= i & -i) ret += bit[i];
+		return ret;
+	}
+	ll query(ll l, ll r) {  // soma [l, r]
+		return pref(r) - pref(l - 1); 
+	}
+	ll upper_bound(ll x) {
+		ll p = 0;
+		for (ll i = __lg(n); i+1; i--) 
+			if (p + (1<<i) <= n and bit[p + (1<<i)] <= x)
+				x -= bit[p += (1 << i)];
+		return p;
+	}
 };
