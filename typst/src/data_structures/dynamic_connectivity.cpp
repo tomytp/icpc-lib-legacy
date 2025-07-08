9fd00a14391f64762a269e3c204f26c38ea0f1d2
joaoa <joaoarthurmarques04@gmail.com>
2025-07-08 19:45:18 -0300

    perseg and dynamic_connectivity

---
+++ typst/src/data_structures/dynamic_connectivity.cpp
@@ -0,0 +1,34 @@
+struct time_query{
+    ll l, r;
+    time_query(ll l_, ll r_){
+        l = l_;
+        r = r_;
+    }
+};
+
+struct time_node {
+    ll lm, rm;
+    unique_ptr<time_node> lc, rc;
+
+    vector<time_query> op;
+
+    time_node(ll lm_, ll rm_){
+        lm = lm_;
+        rm = rm_;
+        if (lm != rm) {
+            ll mid = (lm + rm) / 2;
+            lc = make_unique<time_node>(lm, mid);
+            rc = make_unique<time_node>(mid + 1, rm);
+        }
+    }
+
+    void add_query(ll lq, ll rq, time_query x) {
+        if (rq < lm || lq > rm) return;
+        if (lq <= lm && rm <= rq) {
+            op.push_back(x);
+            return;
+        }
+        lc->add_query(lq, rq, x);
+        rc->add_query(lq, rq, x);
+    }
+};
---
+++ typst/src/data_structures/perseg.cpp
@@ -0,0 +1,82 @@
+struct node {
+    ll lm, rm;
+    ll mn;
+    unique_ptr<node> lc, rc;
+
+    node(ll l, ll r, const vector<ll>& a) : lm(l), rm(r) {
+        if (lm == rm) { 
+            mn = a[lm];
+             return; 
+        }
+
+        ll m = (lm + rm) >> 1;
+        lc = make_unique<node>(lm, m, a);
+        rc = make_unique<node>(m+1, rm, a);
+        pull();
+    }
+
+    static ll comb(ll a, ll b) {
+        return min(a, b);
+    }
+
+    void pull() {
+        mn = comb(lc->mn, rc->mn);
+    }
+
+    void upd(ll lq, ll rq, ll x, vector<pair<node*,ll>>& log) {
+        if (lq > rm || lm > rq) return;
+        if (lq <= lm && rm <= rq) {
+            if (mn < x) {
+                log.emplace_back(this, mn);
+                mn = x;
+            }
+            return;
+        }
+
+        lc->upd(lq, rq, x, log);
+        rc->upd(lq, rq, x, log);
+        
+        ll nxt = comb(lc->mn, rc->mn);
+        
+        if (mn < nxt) {
+            log.emplace_back(this, mn);
+            mn = nxt;
+        }
+    }
+
+    ll get(ll lq, ll rq) const {
+        if (lq > rm || lm > rq) return INF;
+        if (lq <= lm && rm <= rq)  return mn;
+        ll res = min(lc->get(lq, rq), rc->get(lq, rq));
+        return max(res, mn);
+    }
+};
+
+struct segtree {
+    unique_ptr<node> root;
+    vector<pair<node*,ll>> log;
+
+    segtree(const v64& a) {
+        root = make_unique<node>(0, (ll)a.size()-1, a);
+    }
+
+    void upd(ll l, ll r, ll x){
+        root->upd(l, r, x, log);
+    }
+
+    ll get(ll l, ll r){
+        return root->get(l, r);
+    }
+
+    ll version() const {
+        return (ll)log.size();
+    }
+
+    void rollback(ll ver){
+        while ((ll)log.size() > ver){
+            auto [p, old] = log.back();
+            log.pop_back();
+            p->mn = old;
+        }
+    }
+};
